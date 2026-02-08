0b9a4c955239cc1939c7a46b30faec5a37235e80
Joao Vitor <jvrezendemoura@gmail.com>
2026-02-08 08:04:36 -0300

    Craeting more models and tests

--- backend/api/models/subject.py
+++ backend/api/models/subject.py
@@ -1,5 +1,8 @@
 from django.db import models 
+import uuid
 
 
 class Subject(models.Model):
-  pass
+  id =  models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+  knowledge_area = models.CharField(max_length=255, blank=False, null = False)
+  name = models.CharField(max_length = 255, blank=False, null=False)
--- backend/api/tests/test_models.py
+++ backend/api/tests/test_models.py
@@ -437,3 +437,22 @@ class TestSubjectModel(TestCase):
       from api.models.subject import Subject
     except ImportError:
       raise ImportError("Was not possible to import the subject model")
+    
+  def test_if_subject_model_have_correct_superclass(self) -> None:
+    module = importlib.import_module("api.models.subject")
+    class_ = module.Subject 
+    self.assertTrue(issubclass(class_, models.Model))
+    
+  def test_if_model_have_correct_fields(self) -> None:
+    module = importlib.import_module("api.models.subject")
+    class_ = module.Subject 
+    self.assertTrue(hasattr(class_, "id"))
+    self.assertTrue(hasattr(class_, "knowledge_area"))
+    self.assertTrue(hasattr(class_, "name"))
+    
+  def test_if_fields_have_correct_model_datatype(self) -> None:
+    module = importlib.import_module("api.models.subject")
+    class_ = module.Subject 
+    self.assertIsInstance(class_._meta.get_field("id"), models.UUIDField)
+    self.assertIsInstance(class_._meta.get_field("knowledge_area"), models.CharField)
+    self.assertIsInstance(class_._meta.get_field("name"), models.CharField)
