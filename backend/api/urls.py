ff3d912f8f1d46edd0a004c14269771f5939158c
Joao Vitor <jvrezendemoura@gmail.com>
2026-02-12 18:03:07 -0300

    adding teatcher urls for the api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,8 +10,8 @@ router.register(r'users', UserViewSet, basename="users")
 router.register(r'teatchers', TeatcherViewSet, basename="teatchers")
 
 api_urlpatterns = [
-  path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-  path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+  path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+  path('api/auth/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
