36fa505edcaa6d95c3ee46b01300cae5901ab436
Joao Vitor <jvrezendemoura@gmail.com>
2026-02-22 22:04:17 -0300

    adding coyurse

--- backend/api/models/course.py
+++ backend/api/models/course.py
@@ -1,10 +1,10 @@
 from django.db import models
 from .teatcher import Teatcher 
-
+import uuid
 class Course(models.Model):
-  id = models.UUIDField()
-  name = models.CharField()
-  description = models.CharField()
+  id = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid.uuid4)
+  name = models.CharField(max_length=30, editable=True, blank=False, null=False)
+  description = models.CharField(max_length=125, editable=True)
   teatcher = models.ForeignKey(Teatcher, on_delete=models.SET_NULL)
   total_semesters = models.IntegerField()
   actual_semester = models.IntegerField()
