bf31cf8aac561e78da3ca64b54c4fe8125390451
Joao Vitor <jvrezendemoura@gmail.com>
2026-04-10 16:19:22 -0300

    Creating the user service create method

--- backend/users/services/user_service.py
+++ backend/users/services/user_service.py
@@ -2,6 +2,7 @@ from users.models import CustomUser
 from typing import Optional, List 
 from users.models.user import CustomUser
 from uuid import UUID 
+from django.db import transaction
 
 class UserService:
 
@@ -17,9 +18,34 @@ class UserService:
       raise Exception
     
   @staticmethod
-  def create_user() -> CustomUser:
-    pass 
+  @transaction.atomic
+  def create_user(validated_data: dict) -> CustomUser:
+    try:
+      user = CustomUser.objects.create_user(
+        first_name = validated_data["first_name"],
+        last_name = validated_data["last_name"],
+        email = validated_data["email"],
+        password = validated_data["password"]
+      )
+      return user
+    except Exception as e:
+      raise Exception(f"There was a problem with the user creation: {e}")
   
+  @staticmethod 
+  def update_user(instance: CustomUser, validated_data: dict) -> CustomUser:
+    try:
+      if user := UserService._check_user_by_credentials(uuid=instance.id):
+        for attr, value in validated_data.items():
+          user.setattr(attr, value)   
+      else:
+        raise ValueError("There is no user with the given credentials")  
+    
+    except Exception as e:
+      raise Exception(f"There was a problem with the user update: {e}")
+    
+  @staticmethod 
+  def deactivate_user() -> CustomUser:
+    pass  
   
   @staticmethod 
   def _check_user_by_credentials(email: str = None, uuid: UUID = None) -> None:
--- backend/users/tests/unit/user/test_user_service.py
+++ backend/users/tests/unit/user/test_user_service.py
@@ -21,15 +21,44 @@ class TestUserService(TestCase):
     except ImportError:
       raise ImportError("Was not possible to import the user service")
     
+  def test_if_user_service_have_get_all_users_method(self) -> None:
+    module = importlib.import_module("users.services.user_service")
+    class_ = module.UserService
+    self.assertTrue(hasattr(class_, "get_all_users"))
+    
   def test_if_user_service_have_check_credentials_in_database_method(self) -> None:
     module = importlib.import_module("users.services.user_service")
     class_ = module.UserService
+    self.assertTrue(hasattr(class_, "_check_user_by_credentials"))
   
   def test_if_user_service_have_create_user_method(self) -> None:
-    pass 
+    module = importlib.import_module("users.services.user_service")
+    class_ = module.UserService 
+    self.assertTrue(hasattr(class_, "create_user"))
   
   def test_if_user_service_have_update_user_method(self) -> None:
-    pass
+    module = importlib.import_module("users.services.user_service")
+    class_ = module.UserService 
+    self.assertTrue(hasattr(class_, "update_user"))
   
-  def test_if_user_service_have_check_user_by_credentials_method(self) -> None:
-    pass
+  
+  def test_if_user_service_have_deactive_user_method(self) -> None:
+    module = importlib.import_module("users.services.user_service")
+    class_ = module.UserService 
+    self.assertTrue(hasattr(class_, "deactivate_user"))
+
+  def test_if_user_service_create_user_method_works(self) -> None:
+    module = importlib.import_module("users.services.user_service")
+    class_ = module.UserService
+    result = class_.create_user()
+
+  
+  def test_if_user_service_get_all_users_method_works(self) -> None:
+    module = importlib.import_module("users.services.user_service")
+    class_ = module.UserService 
+    self.assertTrue(callable(getattr(class_, "get_all_users", None))) 
+    result = class_.get_all_users()
+    self.assertIsInstance(result, list)
+    self.assertEqual(len(result), 0)
+    
+    
