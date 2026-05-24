ff1143a6ccb73f6f0ad1bfcb15b482c4bdc668e6
Vitor Moura <jvrezendemoura@gmail.com>
2026-05-23 22:10:04 -0300

    Creating more tests

--- backend/courses/services/subject_service.py
+++ backend/courses/services/subject_service.py
@@ -1,6 +1,21 @@
 from courses.models.subject import Subject
+from django.db import transaction
+from typing import List, Optional
+from uuid import uuid4
 
 class SubjectService:
+
+  @staticmethod 
+  def get_all_subjects() -> List[Subject]:
+    return list(Subject.objects.all())
   
-  def create_subject(self) -> Subject:
-    pass
+  @staticmethod 
+  def get_subject_by_id(id: uuid4) -> Optional[Subject]:
+    return Subject.objects.filter(id=id).first()
+  
+  @staticmethod 
+  @transaction.atomic
+  def create_subject(instance: Subject) -> Subject:
+    new_subject = Subject.objects.create(**instance.__dict__)
+    new_subject.save()
+    return new_subject
--- backend/courses/test/unit/subjects/test_subject_service.py
+++ backend/courses/test/unit/subjects/test_subject_service.py
@@ -2,8 +2,36 @@ from django.test import TestCase
 import importlib 
 import inspect 
 from courses.services.subject_service import SubjectService 
-
+from courses.models.subject import Subject
 
 class TestSubjectService(TestCase):
   def setUp(self) -> None:
-    pass
+    self.instance = Subject.objects.create()
+   
+  def test_if_its_running(self) -> None:
+    self.assertTrue(True)
+    
+  def test_if_can_import_the_service(self) -> None:
+    try:
+      from courses.services.subject_service import SubjectService
+      self.assertTrue(inspect.isclass(SubjectService))
+    except ImportError:
+      raise ImportError("Was not possible to import the subject service")
+    
+  def test_if_can_get_all_subjects(self) -> None:
+    module = importlib.import_module("courses.services.subject_service")
+    class_ = module.SubjectService
+    subjects = class_.get_all_subjects()
+    self.assertEqual(len(subjects), 0) 
+    self.assertIsInstance(subjects, list)
+    
+  def test_if_can_get_subject_by_id(self) -> None:
+    module = importlib.import_module("courses.services.subject_service")
+    class_ = module.SubjectService
+    
+    
+    
+  def test_if_can_create_subject_with_class(self) -> None:
+    module = importlib.import_module("courses.services.subject_service")
+    class_ = module.SubjectService
+    new_subject = class_.create_subject(instance=self.instance)
